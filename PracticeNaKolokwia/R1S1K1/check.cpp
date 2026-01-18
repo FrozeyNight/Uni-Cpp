ce94f59bcf44538e215460c49004f4b9e6ac910c
FrozeyNight <frozeynight@gmail.com>
2026-01-18 14:54:01 +0100

    More Kolos Practice

--- PracticeNaKolokwia/R1S1K1/check.cpp
+++ PracticeNaKolokwia/R1S1K1/check.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <cstring>
 
 int main(){
 
     char test = '\0';
     std::cin >> test;
     std::cout << (char)((int)test + 25);
+
+    char* test2 = "fdsfdfsg";
+    std::cout << strlen(test2);
 
     return 0;
 }
